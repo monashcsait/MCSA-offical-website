ede21b575086f96a35984121212ee0588bd9f3cb
Monash Chinese Student Association <monashcsait@gmail.com>
2026-09-18 20:23:18 +1000

    Comment out local API base URL
    Comment out the local API base URL in config.

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 // Local defaults. Run connect.py in the parent folder before production deployment.
 window.MCSA_CONFIG = {
-  apiBase: "http://127.0.0.1:8000/api"
+  apiBase: "" //http://127.0.0.1:8000/api
 };
